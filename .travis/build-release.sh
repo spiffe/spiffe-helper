677a4079fbe53ecee312a9fa2c3760e651131726
Marcos G. Yedro <marcosyedro@gmail.com>
2020-02-13 12:15:45 -0300

    Allow lightweight tags (useful for github releases)
    Signed-off-by: Marcos G. Yedro <marcosyedro@gmail.com>

--- .travis/build-release.sh
+++ .travis/build-release.sh
@@ -2,8 +2,8 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-TAG="$(git describe --abbrev=0 2>/dev/null || true)"
-ALWAYS="$(git describe --always || true)"
+TAG="$(git describe --tags --abbrev=0 2>/dev/null || true)"
+ALWAYS="$(git describe --tags --always || true)"
 
 # Get build number from travis env variable or use "0"
 BUILD="${TRAVIS_BUILD_NUMBER:-0}"
