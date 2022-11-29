7078e69977ab68554c49074b176bad761d0198ec
Faisal Memon <fymemon@yahoo.com>
2022-11-29 12:45:38 -0800

    Proper exiting logic
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>

--- cmd/spiffe-helper/main.go
+++ cmd/spiffe-helper/main.go
@@ -19,6 +19,7 @@ func main() {
 	configFile := flag.String("config", "helper.conf", "<configFile> Configuration file path")
 	flag.Parse()
 
+	log.Infof("Using configuration file: %q\n", *configFile)
 	config, err := ParseConfig(*configFile)
 	if err != nil {
 		log.Errorf("error parsing configuration file: %v\n%v", *configFile, err)
@@ -31,16 +32,18 @@ func main() {
 		log.Warnf("No cmd defined to execute.")
 	}
 
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
-	defer stop()
-
-	log.Infof("Using configuration file: %q\n", *configFile)
-	spiffeSidecar := sidecar.NewSidecar(config)
-
-	err = spiffeSidecar.RunDaemon(ctx)
-	if err != nil {
-		panic(err)
+	if err = runDaemon(config); err != nil {
+		log.Errorf("Exiting due to error: %w", err)
+		os.Exit(1)
 	}
 
 	log.Infof("Exiting")
+}
+
+func runDaemon(config *sidecar.Config) error {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	spiffeSidecar := sidecar.NewSidecar(config)
+	return spiffeSidecar.RunDaemon(ctx)
 }
