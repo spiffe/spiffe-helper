faa1d2fa10c0699f0b0a05fa822def01fd30c0a2
Marcos Yacob <marcos.yacob@hpe.com>
2023-04-13 09:51:07 -0300

    Allow to build windows binaries
    Signed-off-by: Marcos Yacob <marcos.yacob@hpe.com>

---
+++ pkg/sidecar/util_windows.go
@@ -0,0 +1,31 @@
+//go:build windows
+// +build windows
+
+package sidecar
+
+import (
+	"context"
+	"errors"
+
+	"github.com/spiffe/go-spiffe/v2/workloadapi"
+)
+
+// RunDaemon starts the main loop
+// Starts the workload API client to listen for new SVID updates
+// When a new SVID is received on the updateChan, the SVID certificates
+// are stored in disk and a restart signal is sent to the proxy's process
+func (s *Sidecar) RunDaemon(ctx context.Context) error {
+	err := workloadapi.WatchX509Context(ctx, &x509Watcher{sidecar: s}, workloadapi.WithNamedPipeName(s.config.AgentAddress))
+	if err != nil && !errors.Is(err, context.Canceled) {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Sidecar) SignalProcess() error {
+	// Signal to reload certs
+	// TODO: it is not possible to get signal by name on windows,
+	// we must provide int here
+	return errors.New("sending signal is not supported on windows")
+}
