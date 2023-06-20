229c6d6ad5135a61f96f8710f45929abaa51569d
FedeNQ <fedenahuel07@gmail.com>
2023-06-20 15:00:02 -0300

    Adds build-matrix job
    Signed-off-by: FedeNQ <fedenahuel07@gmail.com>

---
+++ .github/tests/run-mysql-test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define the target directory path
+target_dir="../../it"
+
+# Check if the target directory exists
+if [ ! -d "$target_dir" ]; then
+  echo "Error: The target directory '$target_dir' does not exist."
+  exit 1
+fi
+
+# Change to the target directory
+cd "$target_dir" || exit
+
+bash run-mysql-test.sh client 0
+bash run-mysql-test.sh fail 1
