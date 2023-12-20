07f3bf017d359ce00c9a5725e06ac8883fcaaeca
Keegan Witt <keeganwitt@gmail.com>
2023-12-20 10:38:14 -0500

    Replace nil check with len > 0
    Signed-off-by: Keegan Witt <keeganwitt@gmail.com>

--- pkg/sidecar/config.go
+++ pkg/sidecar/config.go
@@ -132,7 +132,7 @@ func ValidateConfig(c *Config) error {
 
 	x509EmptyCount := countEmpty(c.SvidFileName, c.SvidBundleFileName, c.SvidKeyFileName)
 	jwtBundleEmptyCount := countEmpty(c.SvidBundleFileName)
-	if x509EmptyCount == 3 && c.JwtSvids == nil && jwtBundleEmptyCount == 1 {
+	if x509EmptyCount == 3 && len(c.JwtSvids) == 0 && jwtBundleEmptyCount == 1 {
 		return errors.New("at least one of the sets ('svid_file_name', 'svid_key_file_name', 'svid_bundle_file_name'), 'jwt_svids', or 'jwt_bundle_file_name' must be fully specified")
 	}
 
