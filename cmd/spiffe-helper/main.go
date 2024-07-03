4c9ac643cd50f27f40da7d5831c5c90646326c8f
Faisal Memon <fymemon@yahoo.com>
2024-07-03 16:40:27 -0700

    Update cmd/spiffe-helper/main.go
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>
    
    Co-authored-by: Marcos Yacob <marcos.yacob@hpe.com>
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>

--- cmd/spiffe-helper/main.go
+++ cmd/spiffe-helper/main.go
@@ -32,7 +32,7 @@ func startSidecar(log logrus.FieldLogger) error {
 
 	hclConfig, err := config.ParseConfig(configFile)
 	if err != nil {
-		return fmt.Errorf("Failed to parse %q: %w", configFile, err)
+		return fmt.Errorf("failed to parse %q: %w", configFile, err)
 	}
 
 	if err := config.ValidateConfig(hclConfig, daemonModeFlag, log); err != nil {
