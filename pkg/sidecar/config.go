41a12c5796bb878c3d18ca35bc163f338642cac3
Faisal Memon <fymemon@yahoo.com>
2024-08-23 16:36:41 -0700

    add back comment
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>

--- pkg/sidecar/config.go
+++ pkg/sidecar/config.go
@@ -5,6 +5,7 @@ import (
 )
 
 type Config struct {
+	// If true, merge intermediate certificates into Bundle file instead of SVID file.
 	// This is the exptected format for MySQL and some other applications.
 	AddIntermediatesToBundle bool
 
