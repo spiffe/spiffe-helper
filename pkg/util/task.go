6a2734568f72b8c5f2a475f739755712531161b8
Faisal Memon <fymemon@yahoo.com>
2025-03-27 12:03:45 -0700

    Use tasks for managing go routines (#275)
    * Use tasks for managing go routines
    
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>
    
    * Copy code from spire repo
    
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>
    
    * Merge error
    
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>
    
    * Update cmd/spiffe-helper/main.go
    
    Co-authored-by: Marcos Yacob <marcosyacob@gmail.com>
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>
    
    * Fix error handling
    
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>
    
    ---------
    
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>
    Co-authored-by: Marcos Yacob <marcosyacob@gmail.com>

---
+++ pkg/util/task.go
@@ -0,0 +1,82 @@
+package util
+
+import (
+	"context"
+	"fmt"
+	"runtime/debug"
+	"sync"
+)
+
+// RunTasks executes all of the provided functions concurrently and waits for
+// them all to complete. If a function returns an error, all other functions
+// are canceled (i.e. the context they are passed is canceled) and the error is
+// returned. If all functions finish to completion successfully, RunTasks
+// returns nil. If the context passed to RunTasks is canceled then each
+// function is canceled and RunTasks returns ctx.Err(). Tasks passed to
+// RunTasks MUST support cancelation via the provided context for RunTasks to
+// work properly.
+func RunTasks(ctx context.Context, tasks ...func(context.Context) error) error {
+	var wg sync.WaitGroup
+	ctx, cancel := context.WithCancel(ctx)
+	defer func() {
+		cancel()
+		wg.Wait()
+	}()
+
+	errch := make(chan error, len(tasks))
+
+	runTask := func(task func(context.Context) error) (err error) {
+		defer func() {
+			if r := recover(); r != nil {
+				err = fmt.Errorf("panic: %v\n%s\n", r, string(debug.Stack())) //nolint: revive // newlines are intentional
+			}
+			wg.Done()
+		}()
+		return task(ctx)
+	}
+
+	wg.Add(len(tasks))
+	for _, task := range tasks {
+		task := task
+		go func() {
+			errch <- runTask(task)
+		}()
+	}
+
+	for complete := 0; complete < len(tasks); {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case err := <-errch:
+			if err != nil {
+				return err
+			}
+			complete++
+		}
+	}
+
+	return nil
+}
+
+// SerialRun executes all of the provided functions serially.
+// If all functions finish to completion successfully, SerialRun
+// returns nil. If the context passed to SerialRun is canceled
+// then each function is canceled and SerialRun returns ctx.Err().
+// Tasks passed to SerialRun MUST support cancelation via the provided
+// context for SerialRun to work properly.
+func SerialRun(tasks ...func(context.Context) error) func(ctx context.Context) error {
+	return func(ctx context.Context) (err error) {
+		defer func() {
+			if r := recover(); r != nil {
+				err = fmt.Errorf("panic: %v\n%s\n", r, string(debug.Stack())) //nolint: revive // newlines are intentional
+			}
+		}()
+
+		for _, task := range tasks {
+			if err := task(ctx); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+}
