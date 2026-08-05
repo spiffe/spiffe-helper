0b1b17fc653a499dd788bb770a8aaf50377ff68e
Faisal Memon <fymemon@yahoo.com>
2026-08-05 11:30:27 -0700

    Add Go integration test harness (#409)
    * Add Go integration test harness
    
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>
    
    * Inline integration query error assertions
    
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>
    
    * Align integration query test names
    
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>
    
    * Parameterize Postgres setup helpers
    
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>
    
    * Rename compose Load helper to AddFile
    
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>
    
    * Document integration helper APIs
    
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>
    
    * Align MySQL setup helpers with Postgres
    
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>
    
    * Simplify docker compose exec helper
    
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>
    
    * Wait for integration test certificate updates
    
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>
    
    * Clarify docker compose global environment
    
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>
    
    * Use recursive dependabot paths for integration tests
    
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>
    
    * Use setup-go caching for integration workflows
    
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>
    
    ---------
    
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>

---
+++ test/integration/shared_test.go
@@ -0,0 +1,105 @@
+//go:build integration
+
+package integration_test
+
+import (
+	"fmt"
+	"os"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/spiffe/spiffe-helper/test/integration/internal/dockercompose"
+	"github.com/spiffe/spiffe-helper/test/integration/mysql"
+	"github.com/spiffe/spiffe-helper/test/integration/postgres"
+	"github.com/spiffe/spiffe-helper/test/integration/spire"
+	"github.com/stretchr/testify/require"
+)
+
+type integrationEnvironment struct {
+	dockerCompose *dockercompose.Project
+	spire         *spire.Environment
+}
+
+var sharedIntegration struct {
+	once sync.Once
+	env  *integrationEnvironment
+}
+
+var sharedMySQL struct {
+	once sync.Once
+	db   *mysql.Database
+}
+
+var sharedPostgres struct {
+	once sync.Once
+	db   *postgres.Database
+}
+
+func TestMain(m *testing.M) {
+	code := m.Run()
+
+	if sharedIntegration.env != nil {
+		logf := func(format string, args ...any) {
+			fmt.Printf(format+"\n", args...)
+		}
+		sharedIntegration.env.dockerCompose.Close(code != 0, logf)
+		if sharedIntegration.env.spire != nil {
+			sharedIntegration.env.spire.Cleanup(logf)
+		}
+	}
+
+	os.Exit(code)
+}
+
+func integrationEnv(t *testing.T) *integrationEnvironment {
+	t.Helper()
+
+	sharedIntegration.once.Do(func() {
+		dockerCompose := dockercompose.New()
+		sharedIntegration.env = &integrationEnvironment{
+			dockerCompose: dockerCompose,
+		}
+		sharedIntegration.env.spire = spire.Start(t, dockerCompose)
+	})
+
+	require.NotNil(t, sharedIntegration.env, "shared integration environment is required")
+	require.NotNil(t, sharedIntegration.env.spire, "shared SPIRE environment is required")
+	return sharedIntegration.env
+}
+
+func sharedMySQLDB(t *testing.T) (*spire.Environment, *mysql.Database) {
+	t.Helper()
+
+	env := integrationEnv(t)
+	sharedMySQL.once.Do(func() {
+		env.spire.RegisterEntry(t, spire.Entry{
+			SPIFFEID:  mysqlSPIFFEID,
+			Selectors: mysql.Selectors(),
+			DNSNames:  []string{mysqlDNSName},
+			TTL:       time.Minute,
+		})
+		sharedMySQL.db = mysql.Start(t, env.dockerCompose)
+	})
+
+	require.NotNil(t, sharedMySQL.db, "shared MySQL database is required")
+	return env.spire, sharedMySQL.db
+}
+
+func sharedPostgresDB(t *testing.T) (*spire.Environment, *postgres.Database) {
+	t.Helper()
+
+	env := integrationEnv(t)
+	sharedPostgres.once.Do(func() {
+		env.spire.RegisterEntry(t, spire.Entry{
+			SPIFFEID:  postgresSPIFFEID,
+			Selectors: postgres.Selectors(),
+			DNSNames:  []string{postgresDNSName},
+			TTL:       time.Minute,
+		})
+		sharedPostgres.db = postgres.Start(t, env.dockerCompose)
+	})
+
+	require.NotNil(t, sharedPostgres.db, "shared Postgres database is required")
+	return env.spire, sharedPostgres.db
+}
